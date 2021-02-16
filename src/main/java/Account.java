909f3e4baa9c5964af04dcc758a853d6987294fa
Mikkel Dahl Jacobsen <mikkeldahljacobsen@hotmail.com>
2021-02-16 09:15:49 +0100

    Add source code

---
+++ src/main/java/Account.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Account {
+
+    private List<Transaction> transactions;
+    private Customer customer;
+
+    public Account(Customer customer) {
+        this.transactions = new ArrayList<>();
+        this.customer = customer;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public int getBalance(){
+        // TODO: skal debugges
+        int sum = 0;
+        for (Transaction transaction : transactions) {
+            sum += transaction.getAmount();
+        }
+        return sum;
+    }
+
+    public int withDrawAmount(int amount) throws BankException {
+        // TODO: skal kodes og returnere ny saldo. Smid fejl hvis amount > saldo
+        if (getBalance() < amount){
+            throw new BankException("Du forsøger at hæve et beløb som er større end din saldo");
+        } else if (amount <= 0) {
+            throw new BankException("Du kan ikke hæve et beløb som er 0 eller negativt");
+        }
+        transactions.add(new Transaction(-amount, new Date() ));
+        return getBalance() ;
+    }
+
+    public int depositAmount(int amount){
+        // TODO: skal debugges og returnere ny saldo. Smid fejl hvis amount < 0.
+        transactions.add(new Transaction(amount, new Date()));
+        return getBalance();
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+}
---
+++ src/main/java/BankException.java
@@ -0,0 +1,9 @@
+
+// Jons hjemmebryggede exception klasse
+
+public class BankException extends Exception {
+    public BankException(String message) {
+        super(message);
+        // TODO: Gem fejlen i en log (i en fil)
+    }
+}
